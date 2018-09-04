468d4dddb95fc0f2c76cc9d09575d265a18f166c
khalid jamal <kh.jamal95@gmail.com>
2018-09-04 08:01:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function initForm() {
-  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML(template)
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
